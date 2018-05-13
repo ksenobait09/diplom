770ee7fcbe9ba09f57696b6ff02572b5d37ec80e
Nikita <ksenobait_09@mail.ru>
2018-05-13 17:09:07 +0300

    open xls and xlsx

--- BMSTU_test/app/src/main/java/com/ksenobait09/diplom/bmstu_test/MainActivity.java
+++ BMSTU_test/app/src/main/java/com/ksenobait09/diplom/bmstu_test/MainActivity.java
@@ -1,13 +1,17 @@
 package com.ksenobait09.diplom.bmstu_test;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.aspose.cells.LibsLoadHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,31 @@ public class MainActivity extends AppCompatActivity {
         // cIf one wanted to search for ogg vorbis files, the type would be "audio/ogg".
         // To search for all documents available via installed storage providers,
         // it would be "*/*".
-        intent.setType("application/excel");
+        intent.setType("*/*");
+        String[] mimetypes = {"application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
 
         startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i(TAG, "Uri: " + uri.toString());
+            }
+        }
     }
 }
